5dca7455b89492224844c306a29044c3e907c35a
Douglas <dogge20@hotmail.com>
2021-03-21 20:05:52 +0100

    hej

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 
+var mongoose = require('mongoose')
+
+var mongodb = require('mongodb')
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 
 let Person;
 
